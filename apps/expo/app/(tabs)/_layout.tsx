3e2c52a2e05aa66d7083e2c8ff6396c23e599b3b
xementor <ahammedzonaid@gmail.com>
2023-08-09 18:27:37 +0600

    setup expo router

---
+++ apps/expo/app/(tabs)/_layout.tsx
@@ -0,0 +1,54 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { Link, Tabs } from 'expo-router'
+import { Pressable, useColorScheme } from 'react-native'
+
+/**
+ * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+ */
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name']
+  color: string
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme()
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colorScheme == 'light' ? 'red' : 'blue',
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Tab One',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={colorScheme == 'light' ? 'red' : 'blue'}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
